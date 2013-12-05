d2403bbf1030a5c720bceeb3e2176b49e0b2b017
SnooeyNET <sople1@snooey.net>
2013-12-05 13:56:46 +0900

    first commit
    if i commit further days? :)

---
+++ dropzone.dict-ko.js
@@ -0,0 +1,44 @@
+/**
+ * Dropzone Default Language Translate pack
+ *
+ * @lang Korean
+ * @lang-ISO ko-KR
+ *
+ * @author SnooeyNET(sople1@snooey.net) @sople1
+ * @license Public Domain 
+ * @see http://creativecommons.org/publicdomain/mark/1.0/
+ */
+
+Dropzone.prototype.defaultOptions..dictDefaultMessage
+//  = "Drop files here to upload";
+  = "이곳에 파일을 끌어다 놓으세요";
+Dropzone.prototype.defaultOptions..dictFallbackMessage
+//  = "Your browser does not support drag'n'drop file uploads.",
+  = "이 브라우저는 파일을 끌어다 놓아 업로드하는 기능을 사용할 수 없습니다.",
+Dropzone.prototype.defaultOptions..dictFallbackText
+//  = "Please use the fallback form below to upload your files like in the olden days.",
+  = "파일을 업로드하려면 아래에 있는 고전 업로드 기능을 이용해 주세요.",
+Dropzone.prototype.defaultOptions..dictFileTooBig
+//  = "File is too big ({{filesize}}MB). Max filesize: {{maxFilesize}}MB.",
+  = "파일 크기가 너무 큽니다. ({{filesize}}MB) {{maxFilesize}}MB 이하의 파일만 업로드할 수 있습니다.",
+Dropzone.prototype.defaultOptions..dictInvalidFileType
+//  = "You can't upload files of this type.",
+  = "이 파일 형식은 업로드할 수 없습니다.",
+Dropzone.prototype.defaultOptions..dictResponseError
+//  = "Server responded with {{statusCode}} code.",
+  = "서버가 HTTP 응답 코드 {{statusCode}}을 반환했습니다.",
+Dropzone.prototype.defaultOptions..dictCancelUpload
+//  = "Cancel upload",
+  = "업로드 취소",
+Dropzone.prototype.defaultOptions..dictCancelUploadConfirmation
+//  = "Are you sure you want to cancel this upload?",
+  = "이 업로드를 정말 취소하시겠습니까?",
+Dropzone.prototype.defaultOptions..dictRemoveFile
+//  = "Remove file",
+  = "파일 지우기",
+Dropzone.prototype.defaultOptions..dictRemoveFileConfirmation
+//  = null,
+  = null,
+Dropzone.prototype.defaultOptions..dictMaxFilesExceeded
+//  = "You can only upload {{maxFiles}} files.",
+  = "{{maxFiles}}개 이하의 파일만 업로드할 수 있습니다.",
